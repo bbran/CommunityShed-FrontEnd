a077d32d27356bc81b2a37943ea2308364ba402f
Teresa Kolb <teresa.kolb@libertymutual.com>
2017-10-10 14:11:53 -0700

    pulled in datatables for angular and created community shed page

---
+++ src/app/community-shed/community-shed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-shed',
+  templateUrl: './community-shed.component.html',
+  styleUrls: ['./community-shed.component.css']
+})
+export class CommunityShedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from '../sign-up/sign-up.component';
 import { HomepageComponent } from '../homepage/homepage.component';
 import { LogInComponent } from '../log-in/log-in.component';
+import { CommunityShedComponent } from '../community-shed/community-shed.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
   { path: 'homepage',  component: HomepageComponent },
   { path: 'signup',  component: SignUpComponent },
-  { path: 'login',  component: LogInComponent }
+  { path: 'login',  component: LogInComponent },
+  { path: 'communityshed', component: CommunityShedComponent}
 ];
 
 

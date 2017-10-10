369d57975864d84d4e34e65f8422dd4d67eac4b8
Erin <erinflaherty@hotmail.com>
2017-10-10 18:04:02 -0400

    added tables to page

---
+++ src/app/myshed/myshed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myshed',
+  templateUrl: './myshed.component.html',
+  styleUrls: ['./myshed.component.css']
+})
+export class MyshedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

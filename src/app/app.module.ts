65ecaa27509cb134ac3eb35d602c0821097363d6
Teresa Kolb <teresa.kolb@libertymutual.com>
2017-10-10 10:38:55 -0700

    basic html form for creating a new group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewGroupFormComponent } from './new-group-form/new-group-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewGroupFormComponent
   ],
   imports: [
     BrowserModule

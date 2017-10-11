d63622b4cad5db12e4be50e20e8cb7cc55746800
Erin <erinflaherty@hotmail.com>
2017-10-11 19:31:23 -0400

    edits in class by teachers

--- src/app/sign-up.service.ts
+++ src/app/sign-up.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http'
+import 'rxjs/add/operator/map';
 
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SignUpService {
 
-  private url: string = "https://communityshed.herokuapp.com/api/users";
+  private endpoint: string = "https://communityshed.herokuapp.com/api/users";
+  status;
 
   constructor(private http: Http) { }
 
-  createNewUser(userData): Observable<any> {
-    var result;
-    var objectToSend = JSON.stringify(userData);
-  
-    var headers = new Headers();
+  createNewUser(userData: object): Observable<any> {
+    const objectToSend = JSON.stringify(userData);
+    let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-  
-    this.http.post(this.url, objectToSend, { headers: headers })
-       .map((res: Response) => res.json()).subscribe(res => {
-         this.result = res;
-         return this.result;
-       });
+
+    return this.http.post(this.endpoint, objectToSend, { headers: headers })
+      .map(result => result.json())
+
   }
+
+  // createNewUser(userData): Observable<any> {
+  //   var result;
+  //   var objectToSend = JSON.stringify(userData);
+
+  //   var headers = new Headers();
+  //   headers.append('Content-Type', 'application/json');
+
+  //   this.http.post(this.url, objectToSend, { headers: headers })
+  //      .map((res: Response) => res.json())
+  //      .subscribe(res => {
+  //        this.result = res;
+  //        return this.result;
+  //      });
+  // }
+
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SignUpService } from '../sign-up.service';
+import { NgForm } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
+
 export class SignUpComponent implements OnInit {
 
-  result;
+  @ViewChild('signUpForm') userData: NgForm;
 
-  constructor(private signupservice: SignUpService) { }
+  constructor(private signupservice: SignUpService, private router: Router) { }
 
   ngOnInit() {
   }
 
-submitNewUser (){
+submitNewUser(){
+  console.log(this.userData.value)
 
-  this.signupservice.createNewUser(userData) 
-  .subscribe (
-    result => {
-      this.result = result
-      if (result === true) {
-        // route to /communityshed
-      } else {
-        alert ("Email is already reagistered. Please register with unique email.")
-      }
-    }
-  )
+  this.signupservice.createNewUser(this.userData.value) 
+    .subscribe (
+      result => {
+        if (result === true) {
+          alert("User created successfully.")
+          this.router.navigateByUrl('/communityshed');
+        } else {
+          alert ("Email is already reagistered. Please register with unique email.")
+        }
+      },
+      error => console.log(error)
+    )
 
 }
 

d60176b9f481a2d34b722c9623d8435618fbc0b5
Erin <erinflaherty@hotmail.com>
2017-10-13 15:25:30 -0400

    edits to make the component refresh. still not working.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,108 +9,107 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class DataService {
 
-  //all api calls start with this base. concat additional text for endpoint.
+    //all api calls start with this base. concat additional text for endpoint.
 
-  private baseURL: string = "https://communityshed.herokuapp.com/api/";
-  status;
+    private baseURL: string = "https://communityshed.herokuapp.com/api/";
+    status;
 
-  private commonHttpOptions: RequestOptionsArgs;
+    private commonHttpOptions: RequestOptionsArgs;
 
-  //this is necessary for the user authentication to work 'withCredentials'
-  constructor(private http: Http) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    this.commonHttpOptions = { headers, withCredentials: true };
-  }
+    //this is necessary for the user authentication to work 'withCredentials'
+    constructor(private http: Http) {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        this.commonHttpOptions = { headers, withCredentials: true };
+    }
 
-  //Sign up page to create a new user
+    //Sign up page to create a new user
 
-  createNewUser(userData: object): Observable<any> {
-    const objectToSend = JSON.stringify(userData);
+    createNewUser(userData: object): Observable<any> {
+        const objectToSend = JSON.stringify(userData);
 
-    const options: RequestOptionsArgs = {}
+        const options: RequestOptionsArgs = {}
 
-    return this.http
-      .post(this.baseURL + 'users', objectToSend, this.commonHttpOptions)
-      .map(this.extractData)
-      .catch (this.handleError)
-  }
+        return this.http
+            .post(this.baseURL + 'users', objectToSend, this.commonHttpOptions)
+            .map(this.extractData)
+            .catch(this.handleError)
+    }
 
-  //login page 
+    //login page 
 
-  logIn(userData: object): Observable<any> {
-    const objectToSend = JSON.stringify(userData);
+    logIn(userData: object): Observable<any> {
+        const objectToSend = JSON.stringify(userData);
 
-    return this.http
-      .put(this.baseURL + 'sessions/mine', objectToSend, this.commonHttpOptions)
-      .map(this.extractData)
-      .catch (this.handleError)
-  }
+        return this.http
+            .put(this.baseURL + 'sessions/mine', objectToSend, this.commonHttpOptions)
+            .map(this.extractData)
+            .catch(this.handleError)
+    }
 
     //get all community tools
     getCommunityTools(): Observable<any> {
         return this.http
-        .get(this.baseURL + 'tools', this.commonHttpOptions)
-        .map(this.extractData)
-        .catch(this.handleError)
+            .get(this.baseURL + 'tools', this.commonHttpOptions)
+            .map(this.extractData)
+            .catch(this.handleError)
     }
 
     //get to display the groups to which you are a member; myGroups page
     getMyGroups(): Observable<any> {
         return this.http
-        .get(this.baseURL + 'groups', this.commonHttpOptions)
-        .map(this.extractData)
-        .catch(this.handleError)
+            .get(this.baseURL + 'groups', this.commonHttpOptions)
+            .map(this.extractData)
+            .catch(this.handleError)
     }
 
-  //Sign up page to create a new group
+    //Sign up page to create a new group
 
-  createNewGroup(userData: object): Observable<any> {
-    const objectToSend = JSON.stringify(userData);
+    createNewGroup(userData: object): Observable<any> {
+        const objectToSend = JSON.stringify(userData);
 
-    const options: RequestOptionsArgs = {}
+        const options: RequestOptionsArgs = {}
 
-    return this.http
-      .post(this.baseURL + 'groups', objectToSend, this.commonHttpOptions)
-      .map(this.extractData)
-      .catch (this.handleError)
-  }
+        return this.http
+            .post(this.baseURL + 'groups', objectToSend, this.commonHttpOptions)
+            .map(this.extractData)
+            .catch(this.handleError)
+    }
 
-  //get tool details
-  getToolDetails(id): Observable<any> {
-      let apiURL = `${this.baseURL}tools/${id}`
-      return this.http
-        .get(apiURL, this.commonHttpOptions)
-        .map(this.extractData)
-        .catch(this.handleError)
-  }
+    //get tool details
+    getToolDetails(id): Observable<any> {
+        let apiURL = `${this.baseURL}tools/${id}`
+        return this.http
+            .get(apiURL, this.commonHttpOptions)
+            .map(this.extractData)
+            .catch(this.handleError)
+    }
 
-  //success method for all service calls
+    //success method for all service calls
 
-  private extractData(res: Response) {
-    let results = res.json();
-    return results || [];
-  }
+    private extractData(res: Response) {
+        return res.json() || [];
+    }
 
-  // error method for all service calls
+    // error method for all service calls
 
-  private handleError(error: Response | any) {
-    let errMsg: string;
-    if(typeof error._body === "string"){
-        errMsg = error._body
-    }else{
-        if (error instanceof Response) {
-            if(error.status === 0){
-                errMsg = "Error connecting to API"
-            }else{
-                const errorJSON = error.json();
-                errMsg = errorJSON.message;
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (typeof error._body === "string") {
+            errMsg = error._body
+        } else {
+            if (error instanceof Response) {
+                if (error.status === 0) {
+                    errMsg = "Error connecting to API"
+                } else {
+                    const errorJSON = error.json();
+                    errMsg = errorJSON.message;
+                }
             }
         }
-    }
 
-    return Observable.throw(errMsg);
-}
+        return Observable.throw(errMsg);
+    }
 
 
 }

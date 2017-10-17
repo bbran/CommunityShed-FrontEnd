59ad2bb27c0370fc16a4143451080b7691645f2f
Teresa Kolb <teresa.kolb@libertymutual.com>
2017-10-17 09:45:02 -0700

    log out functionality

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataservice: DataService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  logOutUser(){
+    this.dataservice.logOut() 
+      .subscribe (
+        result => {
+          console.log(result);
+          if (result === true) {
+            alert("User logged out successfully.")
+            console.log("successfully logged out")
+            this.router.navigateByUrl('/homepage');
+          } else {
+            alert ("There has been an error.")
+          }
+        },
+        error => console.log('error: ' + JSON.stringify(error))
+      )
+     
+  }
 }

08d0e44abe99017d8bd6389483884e6d997601e6
Teresa Kolb <teresa.kolb@libertymutual.com>
2017-10-18 11:38:56 -0700

    update to get upload file button to appear

--- src/app/addimage/addimage.component.ts
+++ src/app/addimage/addimage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DataService } from '../data.service';
+import { RequestOptions, RequestOptionsArgs } from '@angular/http';
 
 @Component({
   selector: 'app-addimage',
@@ -25,4 +26,27 @@ export class AddimageComponent implements OnInit {
       .switchMap((params: Params) => this.dataservice.getToolDetails(params['id']))
       .subscribe(tool => this.tool = tool);
   }
+
+  fileChange(event) {
+  //   let fileList: FileList = event.target.files;
+  //   if(fileList.length > 0) {
+  //       let file: File = fileList[0];
+  //       let formData:FormData = new FormData();
+  //       formData.append('uploadFile', file, file.name);
+  //       let headers = new Headers();
+  //       headers.append('Content-Type', 'multipart/form-data');
+  //       headers.append('Accept', 'application/json');
+  //       let options = new RequestOptions({ headers: headers });
+  //       this.http.post(`${this.apiEndPoint}`, formData, options)
+  //           .map(res => res.json())
+  //           .catch(error => Observable.throw(error))
+  //           .subscribe(
+  //               data => console.log('success'),
+  //               error => console.log(error)
+  //           )
+  //   }
+  }
+
+  //apiEndPoint = POST "/api/tools/{toolId}/s3/upload"
+  
 }

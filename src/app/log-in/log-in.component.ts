a716ed04da01b8a9e7a5703f30baadf55dab7539
Erin <erinflaherty@hotmail.com>
2017-10-19 16:03:13 -0400

    removed alert on login

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -25,7 +25,6 @@ export class LogInComponent implements OnInit {
       .subscribe (
         result => {
           if (result !== null) {
-            alert("User created successfully.")
             this.router.navigateByUrl('/communityshed');
           } else {
             alert ("Email is already reagistered. Please register with unique email.")
